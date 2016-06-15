647e2911dd1308eb78baceba93562737c38f19ae
jianguowen <wen.jian@cardinfolink.com>
2016-06-15 17:14:11 +0800

    feat:初始化

---
+++ src/components/Gallery.vue
@@ -0,0 +1,18 @@
+<style>
+
+</style>
+
+<template>
+  <div class="gallery-container">
+    <div class="gallery">
+
+    </div>
+    <ul class="trigger">
+
+    </ul>
+  </div>
+</template>
+
+<script>
+
+</script>
